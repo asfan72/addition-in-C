a06974465aa5631206ec2be271b57a8f148eaf98
asfan72 <130361370+asfan72@users.noreply.github.com>
2023-04-10 16:23:33 +0530

    Add files via upload

---
+++ ADDITION.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float a,b,c;
+    printf("Enter value for a and b");
+    scanf("%f%f",&a,&b);
+    c=a+b;
+    printf("addition=%f",c);
+    getch();
+}
